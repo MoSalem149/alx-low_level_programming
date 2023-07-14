c5ce0302894c51a71afa48c97951ae322df57f66
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-07-14 09:45:54 -0700

    4-print_alphabt.c commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: "Print alphabets except q and e"
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		if (n == 101 || n == 113)
+		{
+			n++;
+			continue;
+		}
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
+
