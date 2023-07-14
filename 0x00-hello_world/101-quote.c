f83ca2eee4144385015b40aeae185d711a12d5e3
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-07-13 17:40:02 -0700

    101-quote.c commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,15 +4,14 @@
  * main - Entry point
  *
  * Description: print a qoute using write function
- *        write(int fd,const void *buf,size_t count)
  *
- * Return: (1) (not success)
-*/
+ * Return: 1 (not success)
+ */
 
 int main(void)
 {
-	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char quo[] = "and that piece of art is useful\" - Dora Korpar. 2015-10-19\n";
 
 	write(1, quo, 59);
-	return (1);
+	return (1);
 }
