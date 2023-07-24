28e6e431a4f62f1ced749ff9139edfc1d8c56031
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-07-23 22:38:56 -0700

    4. I can only go one way. I've not got a reverse gear commit

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_re - prints a string, in reverse, followed by a new line.
+ * print_rev - prints a string, in reverse, followed by a new line.
  * Description:  prints a string, in reverse, followed by a new line.
  * @s: string
  * Return: 0
