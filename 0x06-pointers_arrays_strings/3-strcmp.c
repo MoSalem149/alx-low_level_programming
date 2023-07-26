b96c6b02069c2fd60ecb847a8d3640029ec05eab
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-07-26 06:44:17 -0700

    3. strcmp commit

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - compare string values
+ * @s1: input value
+ * @s2: input value
+ * Return: s1[x] - s2[x]
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+        int x;
+
+        x = 0;
+        while (s1[x] != '\0' && s2[x] != '\0')
+        {
+                if (s1[x] != s2[x])
+                {
+                        return (s1[x] - s2[x]);
+                }
+                x++;
+        }
+        return (0);
+}
