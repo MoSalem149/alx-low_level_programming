d6e4df2d1795f243a432a92223fe42b7310decd8
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-07-26 23:57:33 -0700

    3. strcmp commit

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,16 @@
 
 int _strcmp(char *s1, char *s2)
 {
-        int x;
+	int x;
 
-        x = 0;
-        while (s1[x] != '\0' && s2[x] != '\0')
-        {
-                if (s1[x] != s2[x])
-                {
-                        return (s1[x] - s2[x]);
-                }
-                x++;
-        }
-        return (0);
+	x = 0;
+	while (s1[x] != '\0' && s2[x] != '\0')
+	{
+		if (s1[x] != s2[x])
+		{
+			return (s1[x] - s2[x]);
+		}
+		x++;
+	}
+	return (0);
 }
