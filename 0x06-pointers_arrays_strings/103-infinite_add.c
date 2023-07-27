4994dba839b9381b07b3a2e58d4b9158329576ab
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-07-27 01:33:57 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art commit

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -38,59 +38,37 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + x) != '\0')
-	{
 		x++;
-	}
 	while (*(n2 + y) != '\0')
-	{
 		y++;
-	}
 	x--;
 	y--;
 	if (y >= size_r || x >= size_r)
-	{
 		return (0);
-	}
 	while (y >= 0 || x >= 0 || overflow == 1)
 	{
 		if (x < 0)
-		{
 			val1 = 0;
-		}
 		else
-		{
 			val1 = *(n1 + x) - '0';
-		}
 		if (y < 0)
-		{
 			val2 = 0;
-		}
 		else
-		{
 			val2 = *(n2 + y) - '0';
-		}
 		temp_tot = val1 + val2 + overflow;
 		if (temp_tot >= 10)
-		{
 			overflow = 1;
-		}
 		else
-		{
 			overflow = 0;
-		}
 		if (digits >= (size_r - 1))
-		{
 			return (0);
-		}
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
 		y--;
 		x--;
 	}
 	if (digits == size_r)
-	{
 		return (0);
-	}
 	*(r + digits) = '\0';
 	rev_string(r);
 	return (r);
