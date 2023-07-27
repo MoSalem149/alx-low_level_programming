082d0f65aad31b7ccd96e7fcf32768cabfac3002
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-07-27 00:07:31 -0700

    6. Expect the best. Prepare for the worst. Capitalize on what comes commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,17 +6,17 @@
  */
 
 char *cap_string(char *str)
-{       
-        int index = 0;
-        
-        while (str[index])
-        {       
-                while (!(str[index] >= 'a' && str[index] <= 'z'))
-                {       
-                        index++;
-                        if (str[index - 1] == ' ' ||
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+		{
+			index++;
+			if (str[index - 1] == ' ' ||
 					str[index - 1] == '\t' ||
-                                	str[index - 1] == '\n' ||
+					str[index - 1] == '\n' ||
 					str[index - 1] == ',' ||
 					str[index - 1] == ';' ||
 					str[index - 1] == '.' ||
