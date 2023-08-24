11f48cc813f7b8a5aac79d0333d402d7747b048a
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-08-24 07:40:59 -0700

    1. List length commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - Counts the number of elements in a linked list.
+ * @h: Pointer to the list_t list.
+ * Return: The number of elements in the list.
+ */
+size_t list_len(const list_t *h)
+{
+size_t element_count = 0;
+
+while (h)
+{
+	element_count++;
+	h = h->next;
+}
+
+return (element_count);
+}
