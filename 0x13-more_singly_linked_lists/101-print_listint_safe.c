ade0686d8dd4f764c4a4d8f6d577793b93b2116d
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-08-28 13:44:20 -0700

    forth test commit

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,8 +1,7 @@
-#include "lists.h"
 #include <stdio.h>
-
-size_t looped_listint_len(const listint_t *head);
-size_t print_listint_safe(const listint_t *head);
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
 
 /**
  * looped_listint_len - Counts the number of unique nodes
@@ -10,56 +9,32 @@ size_t print_listint_safe(const listint_t *head);
  * Description: task number 12
  * Return:  0 or unique nodes
  */
-size_t looped_listint_len(const listint_t *head)
-{
-	const listint_t *tortoise, *hare;
-	size_t nodes = 0;
-
-	if (head == NULL || head->next == NULL)
-		return (0);
-
-	tortoise = head;
-	hare = head;
-
-	do {
-
-		tortoise = tortoise->next;
-		hare = (hare->next)->next;
-
-	if (!tortoise || !hare)
-		return (0);
-
-	} while (tortoise != hare);
-
-	hare = head;
-
-	while (tortoise != hare)
-	{
-		nodes++;
-		tortoise = tortoise->next;
-		hare = hare->next;
-	}
-
-	return (nodes);
-}
-
-/**
- * print_listint_safe - Prints a listint_t
- * @head: A pointer
- * Description: task number 12
- * Return: The number of nodes
- */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
+	size_t count = 0;
+	const listint_t *slow = head;
+	const listint_t *fast = head;
 
-	nodes = looped_listint_len(head);
-
-	for (; nodes > 0; nodes--)
+	while (fast != NULL && fast->next != NULL)
 	{
-	printf("[%p] %d\n", (void *)head, head->n);
-		head = head->next;
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			printf("-> [%p] %d\n", (void *)slow, slow->n);
+			exit(98);
+		}
+		count++;
 	}
 
-	return (nodes);
+	while (slow != NULL)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		slow = slow->next;
+		count++;
+	}
+
+	return (count);
 }
