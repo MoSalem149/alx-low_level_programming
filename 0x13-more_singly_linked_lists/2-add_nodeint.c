b7f648d072c133137e5aece092c7e426e571ecbb
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-08-28 06:17:33 -0700

    2. Add node commit

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adds a new node at the beginning of the linked lists
+ * @head: pointer to the first node
+ * @n: data to insert in the new node
+ * Description: task number 2
+ * Return: pointer to the new node or null
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
