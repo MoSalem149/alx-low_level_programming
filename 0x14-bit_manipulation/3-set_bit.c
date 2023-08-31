63be502c641001d8353f3d641021d47d28fec6c1
MoSalem149 <mohamedwaelsalem149@gmail.com>
2023-08-31 08:16:45 -0700

    3. 11 commit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * set_bit - sets a bit at a given index to 1
+ * @n: pointer to the num
+ * @index: index of the bit
+ * Description: task number 3
+ * Return: 1 for success or -1 for failure
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+
+	*n = ((1UL << index) | *n);
+	return (1);
+}
